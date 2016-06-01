ae000f8c86496f0944ba49468b9fa346d2217d29
St659 <st659@nas10-240-47-238.york.ac.uk>
2016-06-01 17:01:34 +0100

    Species separation implemented

--- CAZYParser.py
+++ CAZYParser.py
@@ -5,15 +5,15 @@ import sys
 import unicodedata
 import codecs
 
-class myClass:
-    data = []
-    def __init__(self, data):
-        self.data = data
+
 
 def normaliseData(my_list):
     newlist = list()
     for i in my_list:
-        newlist.append(unicodedata.normalize('NFKC', i))
+
+        normalised_string =unicodedata.normalize('NFKC', i)
+        if len(normalised_string) > 2:
+            newlist.append(normalised_string)
     return newlist
 
 
@@ -21,7 +21,7 @@ print (sys.stdout.encoding)
 
 parser = AdvancedHTMLParser()
 
-parser.parseFile('cazyhtml1.txt')
+parser.parseFile('Parsers1/cazyhtml1.txt')
 protein_list = list()
 tax_list = list()
 truncated_row = list()
@@ -34,14 +34,9 @@ linktag_list = parser.getElementsByTagName('a')
 
 row_list = parser.getElementsByTagName('tr')
 
-
-
-
 proteinID = re.compile(r"\w{3}\d{5}.\d{1}")
 
 regex = re.compile(r'\>(.*?)\<')
-
-
 
 for row in row_list:
     if re.search("separateur2", row.innerHTML.strip()):
@@ -51,13 +46,16 @@ for row in row_list:
         tax_list.append(normaliseData(current_row))
 
 
+species_list = list()
+for item in tax_list:
 
+    split_string = item[1].split(' ',2)
+    print(len(split_string))
+    if len(split_string) > 1:
+        species = str.join(split_string[0], split_string[1])
+    else:
+        species = split_string
+    species_list.append(species)
+    new_file.write("%s\n" % item)
 
-
-        #print(tax_list)
-
-print(tax_list)
-
-#for item in tax_list:
-    #new_file.write("%s\n" % item)
-
+print(species_list)
